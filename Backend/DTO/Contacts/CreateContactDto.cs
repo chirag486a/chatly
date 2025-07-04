24259ee7847b20aa90d80ee2c6fc50bc39463023
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2025-07-04 21:09:13 +0545

    Change SendRequest Logic to Create Contact Logic

--- Backend/DTO/Contacts/CreateContactDto.cs
+++ Backend/DTO/Contacts/CreateContactDto.cs
@@ -1,16 +1,26 @@
+using Chatly.Models;
+
 namespace Chatly.DTO.Contacts;
 
 // ----------------------------
 // ➕ 7: Send Friend/Follow Request
 // ----------------------------
-public class SendRequestRequestDto
+
+public class CreateContactRequestDto
 {
     public string? ContactUserId { get; set; }
     public string? ContactUserName { get; set; }
 }
 
-public class SendRequestResponseDto
+public class ContactResponseDto
 {
     public string? Id { get; set; }
-    public string RequestStatus { get; set; } = "Pending"; // or Accepted/Rejected
+    public string? UserId { get; set; }
+    public string? ContactId { get; set; }
+    public string? Status { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public bool ChatDeleted { get; set; }
+    public bool Mutated { get; set; }
+    public bool Archived { get; set; }
+    public int UnreadCount { get; set; }
 }
